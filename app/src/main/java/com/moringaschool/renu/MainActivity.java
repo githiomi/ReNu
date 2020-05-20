c67cd254ffcc387f777cfc4060b8861a8f30e150
Dhosio <danielgithiomi@gmail.com>
2020-05-21 01:02:27 +0300

    Successfully added the loading dialogue

--- app/src/main/java/com/moringaschool/renu/MainActivity.java
+++ app/src/main/java/com/moringaschool/renu/MainActivity.java
@@ -1,14 +1,45 @@
 package com.moringaschool.renu;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlertDialog dialog;
+    Activity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+
+        LayoutInflater inflater = activity.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.activity_loading_dialog, null));
+        builder.setCancelable(false);
+
+        dialog = builder.create();
+        dialog.show();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                dialog.dismiss();
+            }
+        }, 5000);
+        }
+
 }
