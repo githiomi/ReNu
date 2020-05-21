b336d699086f64a1b8a040405b81738251fa6066
Dhosio <danielgithiomi@gmail.com>
2020-05-21 13:42:49 +0300

    Added introduction to my Main Activity

---
+++ app/src/test/java/com/moringaschool/renu/MainActivityTests.java
@@ -0,0 +1,10 @@
+package com.moringaschool.renu;
+
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+
+public class MainActivityTests {
+}
