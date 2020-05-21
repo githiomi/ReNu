0311c31b585ba0f8818fd523059a1891f31deb53
Dhosio <danielgithiomi@gmail.com>
2020-05-21 14:17:49 +0300

    Main activity tests run and pass

--- app/src/test/java/com/moringaschool/renu/MainActivityTests.java
+++ app/src/test/java/com/moringaschool/renu/MainActivityTests.java
@@ -1,10 +1,44 @@
 package com.moringaschool.renu;
 
+import android.content.Intent;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
+import org.robolectric.*;
+import org.robolectric.shadows.ShadowActivity;
+
+import static org.junit.Assert.*;
 
 @RunWith(RobolectricTestRunner.class)
 
 public class MainActivityTests {
+
+    private MainActivity mainActivity;
+
+    @Before
+    public void setUp(){
+        mainActivity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void checksThatMyAppMainActivityHasDesiredText() {
+        TextView appName = mainActivity.findViewById(R.id.tvAppName);
+        assertEquals("RE-NU", appName.getText().toString());
+    }
+
+    @Test
+    public void checksToSeeIfItOpensTheNextIntent(){
+        mainActivity.findViewById(R.id.btnProceed).performClick();
+        Intent expected = new Intent(mainActivity, LoginActivity.class);
+
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(mainActivity);
+        Intent actual = shadowActivity.getNextStartedActivity();
+
+        assertTrue(actual.filterEquals(expected));
+    }
+
+
+
 }
