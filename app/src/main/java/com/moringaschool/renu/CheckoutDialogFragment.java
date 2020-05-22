e985d8e22b4a80afbc5b923d889ffe0f078c91be
Dhosio <danielgithiomi@gmail.com>
2020-05-22 16:56:36 +0300

    Final week 1 commit

--- app/src/main/java/com/moringaschool/renu/CheckoutDialogFragment.java
+++ app/src/main/java/com/moringaschool/renu/CheckoutDialogFragment.java
@@ -1,17 +1,39 @@
 package com.moringaschool.renu;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.DialogFragment;
 
-public class CheckoutDialogFragment extends DialogFragment {
+public class CheckoutDialogFragment extends DialogFragment{
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_checkout_dialogue, container, false);
+        View mainView = inflater.inflate(R.layout.activity_checkout_dialogue, container, false);
         getDialog().setTitle("Dialogue");
-        return rootView;
+
+        Button complete = (Button) mainView.findViewById(R.id.btnComplete);
+
+        complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        View main = inflater.inflate(R.layout.activity_loading_dialog, container, false);
+                        dismiss();
+                    }
+                }, 1000);
+            }
+        });
+
+        return mainView;
     }
+
 }
