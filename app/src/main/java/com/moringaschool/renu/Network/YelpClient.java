ee161742842b78a9308f680fa68dac0ae3a2f2f1
Dhosio <danielgithiomi@gmail.com>
2020-05-29 19:53:32 +0300

    Added the okhttp3 interceptor

--- app/src/main/java/com/moringaschool/renu/Network/YelpClient.java
+++ app/src/main/java/com/moringaschool/renu/Network/YelpClient.java
@@ -1,6 +1,7 @@
 package com.moringaschool.renu.Network;
 
-import com.moringaschool.myrestaurant.models.Constants;
+
+import com.moringaschool.renu.models.ApiConstants;
 
 import java.io.IOException;
 
@@ -8,6 +9,7 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -22,25 +24,21 @@ public class YelpClient {
 
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-//            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).addInterceptor(interceptor1);
-
-
-//            OkHttpClient okHttpClient = new OkHttpClient.Builder();
 
             Interceptor interceptor1 = new Interceptor() {
-                        @Override
-                        public Response intercept(Chain chain) throws IOException {
-                            Request newRequest  = chain.request().newBuilder()
-                                    .addHeader("Authorization", "Bearer " + "b3DAtEi7MimM3NfdE4OuKromHFxMCjmyk95qKvuUlo603FZdvVheXjC1KU2FME5k1suyd2uFdkNsC6RnHad1Y4fbOjXABgr_SMrU9o0hrQ10mYE5zIKWx3_PYX_LXnYx")
-                                    .build();
-                            return chain.proceed(newRequest);
-                        }
-                    };
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request newRequest  = chain.request().newBuilder()
+                            .addHeader("Authorization", "Bearer " + "b3DAtEi7MimM3NfdE4OuKromHFxMCjmyk95qKvuUlo603FZdvVheXjC1KU2FME5k1suyd2uFdkNsC6RnHad1Y4fbOjXABgr_SMrU9o0hrQ10mYE5zIKWx3_PYX_LXnYx")
+                            .build();
+                    return chain.proceed(newRequest);
+                }
+            };
 
             OkHttpClient.Builder client = new OkHttpClient.Builder().addInterceptor(interceptor).addInterceptor(interceptor1);
 
             retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.YELP_BASE_URL)
+                    .baseUrl(ApiConstants.YELP_BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(client.build())
                     .build();
