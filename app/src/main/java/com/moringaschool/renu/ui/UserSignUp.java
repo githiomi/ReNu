dec17ec4d7261715874ca442bc7745026fdcab90
Dhosio <danielgithiomi@gmail.com>
2020-06-03 01:25:33 +0300

    Created a new image asset for the @ for emails

---
+++ app/src/main/java/com/moringaschool/renu/ui/UserSignUp.java
@@ -0,0 +1,14 @@
+package com.moringaschool.renu.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.moringaschool.renu.R;
+import android.os.Bundle;
+
+public class UserSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_sign_up);
+    }
+}
