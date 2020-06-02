d40f78f0339fb9900e620dee0049c0428eca7046
Dhosio <danielgithiomi@gmail.com>
2020-06-03 00:34:51 +0300

    Created the layout file for the user login activity

--- app/src/main/java/com/moringaschool/renu/ui/UserLogin.java
+++ app/src/main/java/com/moringaschool/renu/ui/UserLogin.java
@@ -3,14 +3,35 @@ package com.moringaschool.renu.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.moringaschool.renu.R;
 
 public class UserLogin extends AppCompatActivity {
 
+//    TAG
+    private static final String TAG = UserLogin.class.getSimpleName();
+
+//    B
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+
+        mPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                hideKeyboard(v);
+            }
+        });
+
+    }
+
+//    Method to hide keyboard
+    public void hideKeyboard(View v){
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 }
